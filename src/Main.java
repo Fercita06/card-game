f91cf912595fb1f09d152347cc684363e678be17
suarezfer1712 <suarezfer1712@gmail.com>
2022-07-11 22:46:50 +0100

    Added the sort method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+
+
         CardGame cardGame = new CardGame();
         cardGame.populateDeck();
+
+
+        cardGame.getFirstCard();
+        cardGame.shuffleDeck();
+        cardGame.sortDeckInNumberOrder();
+        cardGame.sortDeckIntoSuits();
+
     }
 }
+
+
